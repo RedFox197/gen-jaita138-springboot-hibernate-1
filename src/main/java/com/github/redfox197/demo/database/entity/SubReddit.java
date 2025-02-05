cb9055952084d455cdd25484091ec4376012fe64
RedFox197 <190109902+RedFox197@users.noreply.github.com>
2025-02-05 23:55:33 +0100

    aggiunta entity subreddit

---
+++ src/main/java/com/github/redfox197/demo/database/entity/SubReddit.java
@@ -0,0 +1,58 @@
+package com.github.redfox197.demo.database.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class SubReddit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 64)
+    private String name;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    public SubReddit() {
+
+    }
+
+    public SubReddit(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "SubReddit [id=" + id + ", name=" + name + ", description=" + description + "]";
+    }
+}
